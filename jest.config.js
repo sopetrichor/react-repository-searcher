7f936034e9fce6a48d24bb8f036f236e40744731
sopetrichor <stcwer@gmail.com>
2021-06-04 05:27:28 +0800

    add jest config

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    preset: 'ts-jest',
+    globals: {
+        'ts-jest': {
+            tsConfig: 'tsconfig.jest.json',
+        },
+    },
+    moduleNameMapper: {
+        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/__mocks__/fileMock.js',
+        '\\.(css|less)$': '<rootDir>/__mocks__/styleMock.js',
+        '^@components/(.*)$': '<rootDir>/src/components/$1',
+        '^@containers/(.*)$': '<rootDir>/src/containers/$1',
+        '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^@models/(.*)$': '<rootDir>/src/models/$1',
+        '^@services/(.*)$': '<rootDir>/src/services/$1',
+        '^@src/(.*)$': '<rootDir>/src/$1',
+    },
+};
