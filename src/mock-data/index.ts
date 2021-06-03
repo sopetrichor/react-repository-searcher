b71035f54a9c1f7d677ecac1f097c834d4d160a8
sopetrichor <stcwer@gmail.com>
2021-06-04 05:47:05 +0800

    add mock data

---
+++ src/mock-data/index.ts
@@ -0,0 +1,2 @@
+export { default as MOCK_DATA_SUCCESS_EMPTY_RESULTS } from './successEmptyResults';
+export { default as MOCK_DATA_SUCCESS_RESULTS } from './successResults';
---
+++ src/mock-data/successEmptyResults.ts
@@ -0,0 +1,7 @@
+const mockData = {
+    total_count: 0,
+    incomplete_results: false,
+    items: [],
+};
+
+export default mockData;
