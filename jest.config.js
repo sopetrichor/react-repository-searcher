6fcc8b2d1ddf177277bca4965dd1a87e418fa9db
sopetrichor <stcwer@gmail.com>
2021-06-06 16:39:09 +0800

    add jest-extended and alter related config files

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
             tsConfig: 'tsconfig.jest.json',
         },
     },
+    setupFilesAfterEnv: ['jest-extended'],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
             '<rootDir>/__mocks__/fileMock.js',
@@ -12,6 +13,7 @@ module.exports = {
         '^@components/(.*)$': '<rootDir>/src/components/$1',
         '^@containers/(.*)$': '<rootDir>/src/containers/$1',
         '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+        '^@mock-data/(.*)$': '<rootDir>/src/mock-data/$1',
         '^@models/(.*)$': '<rootDir>/src/models/$1',
         '^@services/(.*)$': '<rootDir>/src/services/$1',
         '^@src/(.*)$': '<rootDir>/src/$1',
